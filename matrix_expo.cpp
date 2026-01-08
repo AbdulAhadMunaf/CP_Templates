d5125207297fe6c0359cf10bade2c8ca897583d9
abdul_ahad4 <abdulahadmunaf4@gmail.com>
2026-01-08 16:40:21 +0500

    added matrix expo template

---
+++ matrix_expo.cpp
@@ -0,0 +1,32 @@
+vector<vi> multiply(vector<vi> &a, vector<vi> &b)
+{
+    int n = a.size();
+    vector<vi> res(n, vi(n, inf));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                res[i][j] = min(res[i][j], a[i][k] + b[k][j]);
+            }
+        }
+    }
+    return res;
+}
+ 
+vector<vi> matrix_expo(vector<vi> v, int p)
+{
+    int n = v.size();
+    vector<vi> res(n, vi(n, 0));
+ 
+    while (p)
+    {
+        if (p & 1)
+            res = multiply(res, v);
+        v = multiply(v, v);
+        p >>= 1;
+    }
+    return res;
+}
+ 
