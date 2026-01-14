5435cf7655ec955b6ff486937d83527ae068e390
abdul_ahad4 <abdulahadmunaf4@gmail.com>
2026-01-14 16:08:29 +0500

    Removed binpow, as it's not needed from matrix expo

--- matrix_expo.cpp
+++ matrix_expo.cpp
@@ -1,17 +1,4 @@
 
-int binpow(int a, int b, int m)
-{
-    a %= m;
-    int res = 1;
-    while (b > 0)
-    {
-        if (b & 1)
-            res = res * a % m;
-        a = a * a % m;
-        b >>= 1;
-    }
-    return res;
-}
 vector<vi> multiply(vector<vi> &a, vector<vi> &b)
 {
     int n = a.size();
